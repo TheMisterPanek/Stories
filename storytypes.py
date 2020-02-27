ca8302ce5992916a31a952a07c3a40bc2c9aec52
Ponkrashov <mrpanek@yandex.by>
2020-02-27 21:13:08 +0000

    Correct inherit and add creating tables

--- storytypes.py
+++ storytypes.py
@@ -2,22 +2,33 @@ from peewee import *
 
 db = SqliteDatabase('people.db')
 
-class Entity():
-   class Meta:
-        database = db
-
-class User(Entity):
+class User(Model):
   chat_id = IntegerField()
 
-class Leaf(Entity):
+  class Meta:
+    database = db
+
+class Leaf(Model):
   leaf_type = CharField()
   content = CharField()
 
-class Story(Entity):
+  class Meta:
+    database = db
+
+class Story(Model):
   name = CharField()
   user = ForeignKeyField(User)
 
-class Tree(Entity):
+  class Meta:
+    database = db
+
+class Tree(Model):
   story = ForeignKeyField(Story)
   parent = ForeignKeyField(Leaf)
   child = ForeignKeyField(Leaf)
+
+  class Meta:
+    database = db
+
+if db.connect():
+  db.create_tables([User, Leaf, Story, Tree])
